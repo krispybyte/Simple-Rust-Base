8c33d06f7e18de53f90d5fa97ae282388f97c3fb
peiga <69155363+peiga@users.noreply.github.com>
2022-06-03 01:46:18 +0300

    update offset file (03/06/22)

--- sdk/offsets.hpp
+++ sdk/offsets.hpp
@@ -8,8 +8,12 @@ namespace o
 	rust_offset GameObjectManager = 0x17C1F18;
 
 	// Script.json
-	rust_offset BaseNetworkable_c = 0x3101700;
-	rust_offset Graphics_c = 0x30FF330;
+	rust_offset BaseEntity_c = 0x3216C50; // BaseEntity_c
+	rust_offset BaseNetworkable_c = 0x32178A8; // BaseNetworkable_c
+	rust_offset Graphics_c = 0x32494C8;
+
+	// camera
+	rust_offset MainCamera_c = 0x3218808;
 
 	// Graphics
 	rust_offset grassshadows = 0x10;
@@ -21,22 +25,22 @@ namespace o
 
 	// Base Player
 	rust_offset BasePlayer_modelstate = 0x5F8;
-	rust_offset BasePlayer_playerFlags = 0x680;
-	rust_offset BasePlayer_playerModel = 0x4C0;
-	rust_offset BasePlayer_playerInput = 0x4e0;
-	rust_offset BasePlayer_playerWalkMovement = 0x4E8;
+	rust_offset BasePlayer_playerFlags = 0x688;
+	rust_offset BasePlayer_playerModel = 0x4C8;
+	rust_offset BasePlayer_playerInput = 0x4E8;
+	rust_offset BasePlayer_playerWalkMovement = 0x4F0;
 	rust_offset BasePlayer_clientTickInterval = 0x650;
-	rust_offset BasePlayer_displayName = 0x6E0;
-	rust_offset BasePlayer_eyes = 0x688;
-	rust_offset BasePlayer_inventory = 0x690;
-	rust_offset BasePlayer_needsClothesRebuild = 0x520;
+	rust_offset BasePlayer_displayName = 0x6E8;
+	rust_offset BasePlayer_eyes = 0x690;
+	rust_offset BasePlayer_inventory = 0x698;
+	rust_offset BasePlayer_needsClothesRebuild = 0x528;
 	rust_offset BasePlayer_lastSentTickTime = 0x654;
 	rust_offset BasePlayer_mounted = 0x600;
 
 	// Base Combat Entity
-	rust_offset BaseCombatEntity_health = 0x224;
-	rust_offset BaseCombatEntity_maxHealth = 0x228;
-	rust_offset BaseCombatEntity_lifestate = 0x21C;
+	rust_offset BaseCombatEntity_health = 0x22C;
+	rust_offset BaseCombatEntity_maxHealth = 0x230;
+	rust_offset BaseCombatEntity_lifestate = 0x224;
 
 	// Model State
 	rust_offset ModelState_waterLevel = 0x14;
@@ -54,12 +58,12 @@ namespace o
 	rust_offset PlayerWalkMovement_capsuleCenter = 0x6C;
 
 	// Player Model
-	rust_offset PlayerModel_newVelocity = 0x22C;
-	rust_offset PlayerModel_isLocalPlayer = 0x289;
-	rust_offset PlayerModel_MaleSkin = 0x190;
-	rust_offset PlayerModel_FemaleSkin = 0x198;
-	rust_offset PlayerModel_IsNPC = 0x310;
-	rust_offset PlayerModel_visible = 0x278;
+	rust_offset PlayerModel_newVelocity = 0x23C;
+	rust_offset PlayerModel_isLocalPlayer = 0x299;
+	rust_offset PlayerModel_MaleSkin = 0x1A0;
+	rust_offset PlayerModel_FemaleSkin = 0x1A8;
+	rust_offset PlayerModel_IsNPC = 0x320;
+	rust_offset PlayerModel_visible = 0x288;
 
 	// Player Input
 	rust_offset PlayerInput_bodyAngles = 0x3C;
@@ -75,20 +79,20 @@ namespace o
 	rust_offset Projectile_thickness = 0x2C;
 
 	// Base Projectile
-	rust_offset BaseProjectile_automatic = 0x288;
-	rust_offset BaseProjectile_canUnloadAmmo = 0x2B4;
-	rust_offset BaseProjectile_primaryMagazine = 0x2B8;
-	rust_offset BaseProjectile_aimSway = 0x2D0;
-	rust_offset BaseProjectile_aimCone = 0x2E8;
-	rust_offset BaseProjectile_recoil = 0x2D8;
-	rust_offset BaseProjectile_aiming = 0x309;
-	rust_offset BaseProjectile_isReloading = 0x324;
-	rust_offset BaseProjectile_createdProjectiles = 0x358;
+	rust_offset BaseProjectile_automatic = 0x290;
+	rust_offset BaseProjectile_canUnloadAmmo = 0x2BC;
+	rust_offset BaseProjectile_primaryMagazine = 0x2C0;
+	rust_offset BaseProjectile_aimSway = 0x2D8;
+	rust_offset BaseProjectile_aimCone = 0x2F0;
+	rust_offset BaseProjectile_recoil = 0x2E0;
+	rust_offset BaseProjectile_aiming = 0x311;
+	rust_offset BaseProjectile_isReloading = 0x340;
+	rust_offset BaseProjectile_createdProjectiles = 0x370;
 
 	// Magazine
-	rust_offset Magazine_capacity = 0x18;
-	rust_offset Magazine_contents = 0x1C;
-	rust_offset Magazine_ammoType = 0x20;
+	rust_offset Magazine_capacity = 0x14;
+	rust_offset Magazine_contents = 0x18;
+	rust_offset Magazine_ammoType = 0x1C;
 
 	// Recoil Properties
 	rust_offset RecoilProperties_recoilYawMin = 0x18;
@@ -99,10 +103,10 @@ namespace o
 	rust_offset RecoilProperties_movementPenalty = 0x34;
 
 	// Item
-	rust_offset Item_info = 0x20;
-	rust_offset Item_uid = 0x28;
-	rust_offset Item_amount = 0x30;
-	rust_offset Item_heldEntity = 0x98;
+	rust_offset Item_info = 0x20; // no "info"
+	rust_offset Item_uid = 0x14;
+	rust_offset Item_amount = 0x20;
+	rust_offset Item_heldEntity = 0x40;
 
 	// Inventory
 	rust_offset Inventory_containerMain = 0x20;
@@ -112,8 +116,8 @@ namespace o
 	rust_offset Inventory_loot = 0x40;
 
 	// BaseMountable
-	rust_offset BaseMountable_pitchClamp = 0x2A0;
-	rust_offset BaseMountable_yawClamp = 0x2A8;
-	rust_offset BaseMountable_canWieldItems = 0x2B0;
-	rust_offset BaseMountable_canDrinkWhileMounted = 0x308;
+	rust_offset BaseMountable_pitchClamp = 0x2A8;
+	rust_offset BaseMountable_yawClamp = 0x2B0;
+	rust_offset BaseMountable_canWieldItems = 0x2B8;
+	rust_offset BaseMountable_canDrinkWhileMounted = 0x310;
 }
